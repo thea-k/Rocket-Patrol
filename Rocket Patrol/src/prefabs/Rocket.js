ad7a79dd36e2945342c37b0fa7e29e54226791ef
thea-k <theaknasiak@gmail.com>
2021-04-10 16:02:35 -0700

    added rocket firing controls

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -3,20 +3,33 @@ class Rocket extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.MovementSpeed = 2;
+        this.isFiring = false;
     }
 
     update() {
-        if(keyLeft.isDown) {
-            this.x -= this.MovementSpeed;
-        }
-        if(keyRight.isDown) {
-            this.x += this.MovementSpeed;
-        }
+        if(this.isFiring) {
+            this.y -= this.MovementSpeed;
+            if(this.y < borderUISize*3) {
+                this.y = game.config.height-borderUISize-borderPadding;
+                this.isFiring = false;
+            }
+        } else {
+            if(keyLeft.isDown) {
+                this.x -= this.MovementSpeed;
+            }
+            if(keyRight.isDown) {
+                this.x += this.MovementSpeed;
+            }
+            
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            }
 
-        //restrict rocket movement to remain in bounds of game window
-        this.x = Phaser.Math.Clamp(
-            this.x, 
-            borderUISize+borderPadding, 
-            game.config.width-borderUISize-borderPadding);
+            //restrict rocket movement to remain in bounds of game window
+            this.x = Phaser.Math.Clamp(
+                this.x, 
+                borderUISize+borderPadding, 
+                game.config.width-borderUISize-borderPadding);
+        }
     }
 }
