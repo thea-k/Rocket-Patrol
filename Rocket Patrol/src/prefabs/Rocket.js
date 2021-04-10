bad3c3b8467e24f55edb658039a8a7033787f02b
thea-k <theaknasiak@gmail.com>
2021-04-10 16:15:46 -0700

    changed movement speed

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -2,7 +2,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-        this.MovementSpeed = 2;
+        this.MovementSpeed = 3;
         this.isFiring = false;
     }
 
